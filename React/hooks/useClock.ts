8b642bd2217e7f94f8f75f6d2d63109d8e353ec1
hyunjin <leehj0110@kakao.com>
2021-11-23 23:28:29 +0900

    useClock

---
+++ React/hooks/useClock.ts
@@ -0,0 +1,14 @@
+import React ,{useState, useEffect} from 'react'
+
+export const useClock = () => {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    const id = setInterval(() => {
+      setTime(new Date())
+    }, 1000)
+    return () => clearInterval(id)
+  },[])
+
+  return time
+}
