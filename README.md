3ea7327ada4108694bb7c234501b14cbfbfe43e6
hyunjin <leehj0110@kakao.com>
2022-05-22 14:56:46 +0900

    chore: 폴더 정리, 글 작성 방식 변경

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-<!-- # <h1 style='border-bottom:17px solid black'>Today I Learned</h1>
-
-<h1 style='border-bottom:17px solid lightblue'>가보자가보자~</h1>
-zz -->
-
 # Today I Learned
-
-<Home/>
--- operating_system/docs/code/process_creation3.c
+++
@@ -1,9 +0,0 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-int main() {
-	printf("execute ls\n");
-	execl("/bin/ls", "ls", "-l", NULL); // ls -l 실행
-	perror("execl is failed\n"); // 에러 출력 (execl로 실행 공간을 덮어 씌웠기 때문에 출력되지 않는다.)
-	exit(1);
-}
